1a91bff6df2b71bf5f161055d41d8e7b76759443
Chithraj <chithraj23102003@gmail.com>
2022-05-05 16:19:08 +0530

    Added_some_files

--- Find.java
+++ Find.java
@@ -1,12 +1,3 @@
-public class Find
-{
-
-public static void main(String[] args) {
-   int arr[] = {1,2,3,4,5};
-   for ( int i = 0; i < arr.length - 2 ; ++i)
-   System.out.println(arr[i] + " ");
-}
-}
 
 
     
---
+++ com/testpackage/chith.java
@@ -0,0 +1,12 @@
+package com.testpackage;
+
+ class chith {
+
+}
+class malai{
+
+}
+
+class sangi {
+
+}
